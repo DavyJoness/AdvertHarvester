01ec5bc38fe94d63ef5c66feb9763605fa004d47
Mateusz Kiełpiński <mateusz.kielpinski@profisoft.pl>
2020-02-06 15:30:55 +0100

    utworzono tabele

--- Zboeracz/skrypt.sql
+++ Zboeracz/skrypt.sql
@@ -1,3 +1,11 @@
+create table dbo.SearchLink
+(
+	Sel_Id int identity(1,1) primary key,
+	Sel_Url varchar(768) not null unique,
+	Sel_Name varchar(512),
+	Sel_Describe varchar(2048)
+);
+
 create table dbo.Advert
 (
 	Adw_Id int identity(1,1) primary key,
@@ -10,7 +18,33 @@ create table dbo.Advert
 	Adw_Url varchar(512) not null,
 	Adw_IsPromoted bit default(0),
 	Adw_CreateDate datetime default(GETDATE()),
-)
+	Adw_SeLId int foreign key references dbo.SearchLink (Sel_Id)
+);
+
+create table dbo.Attributes
+(
+	Atr_Id int identity(1,1) primary key,
+	Atr_AdwId int foreign key references dbo.Advert (Adw_Id),
+	Atr_Name varchar(255) not null,
+	Atr_Value varchar(255) not null
+);
+
+create table dbo.Describe
+(
+	Des_Id int identity(1,1) primary key,
+	Des_AdwId int foreign key references dbo.Advert (Adw_Id),
+	Des_Describe varchar(8000),
+	Des_Exposer varchar(32),
+	Des_Tel varchar(9)
+);
+
+create table dbo.Pictures
+(
+	Pic_Id int identity(1,1) primary key,
+	Pic_AdwId int foreign key references dbo.Advert (Adw_Id),
+	Pic_Content binary
+);
+
 --drop table dbo.Advert
 select iif(exists(select 1 from dbo.Advert where Adw_ForeignId = @Id),1,0) IsAdwExists
 
